e34f38719500b7285353476405056a7d6921c76c
Luis VP <luisvalles92@hotmail.com>
2021-01-31 17:43:14 +0100

    Primer commit del Proyecto

---
+++ vacunas_covid/src/main/java/com/luisvp/vacunas_covid/VacunasCovidApplication.java
@@ -0,0 +1,13 @@
+package com.luisvp.vacunas_covid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacunasCovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacunasCovidApplication.class, args);
+	}
+
+}
---
+++ vacunas_covid/src/test/java/com/luisvp/vacunas_covid/VacunasCovidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisvp.vacunas_covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacunasCovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
